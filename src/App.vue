bc2745219e4a32cad788cece63b7cd2fcc1284d7
Beat <beat@xiyanghui.com>
2017-03-01 21:31:51 +0800

    new project

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+     <div class="header">
+       我是头
+     </div>
+     <div class="tab-link">
+         <router-link to="/goods">qu goods</router-link>
+     </div>
+     <div class="content">
+       我是内容
+     </div>
+
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+};
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
